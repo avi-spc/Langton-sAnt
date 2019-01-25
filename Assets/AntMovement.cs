483eff05dabd3d2e037db6f97d0e0bfbd081d877
Avdhesh Yadav <avdhesh7394vn@gmail.com>
2019-01-25 17:19:28 +0530

    Init

---
+++ Assets/AntMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntMovement : MonoBehaviour
+{
+    Color unitColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        //    transform.Rotate(new Vector3(0, 90, 0));
+        //    transform.Translate(0, 0, 1);
+        //}
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag.Equals("Square")) {
+            float r = col.gameObject.GetComponentInParent<Renderer>().material.color.r;
+            float g = col.gameObject.GetComponentInParent<Renderer>().material.color.g;
+            float b = col.gameObject.GetComponentInParent<Renderer>().material.color.b;
+            float a = col.gameObject.GetComponentInParent<Renderer>().material.color.a;
+
+            if (r == 1f && g == 1f && b == 1f && a == 1f) {
+                // transform.Translate(new Vector3(1, 0, 1) * Time.deltaTime * 3f);
+                transform.Rotate(new Vector3(0, 90, 0));
+                transform.Translate(0, 0, 1);
+                col.gameObject.GetComponentInParent<Renderer>().material.color = Color.red;
+            }
+
+            if (r == 1f && g == 0f && b == 0f && a == 1f) {
+                //transform.Translate(new Vector3(0, 0, 1));
+                transform.Rotate(new Vector3(0, -90, 0));
+                transform.Translate(0, 0, 1);
+                col.gameObject.GetComponentInParent<Renderer>().material.color = Color.white;
+            }
+            Debug.Log(unitColor);
+        }
+    }
+}
---
+++ Assets/PlatformGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    Vector3[] vertices;
+
+    public int xSize;
+    public int zSize;
+
+    public GameObject platformUnit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GeneratePlatform();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GeneratePlatform() {
+        vertices = new Vector3[xSize * zSize];
+        for (int i=0, z = 0; z < zSize; z++) {
+            for (int x = 0; x < xSize; x++) {
+                vertices[i] = new Vector3(x, 0, z);
+                GameObject platUnit = Instantiate(platformUnit, vertices[i], Quaternion.Euler(90, 0, 0));
+                i++;
+            }
+        }
+
+        
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < vertices.Length; i++) {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
